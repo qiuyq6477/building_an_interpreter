48f0868b14590bbac5ab2ec63fa087bb124ca067
zj <819685619@qq.com>
2022-10-11 14:35:03 +0800

    add variables

--- Eva.js
+++ Eva.js
@@ -1,7 +1,10 @@
 const assert = require("assert");
-
+const Environment = require("./Environment")
 class Eva {
-    eval(exp){
+    constructor(global = new Environment()){
+        this.global = global;
+    }
+    eval(exp, env = this.global){
         // -----------------------------
         // self-evaluating expressions
     
@@ -16,18 +19,29 @@ class Eva {
         // -----------------------------
         // math operations
     
-        if (exp[0] == "+"){
+        if (exp[0] === "+"){
             return this.eval(exp[1]) + this.eval(exp[2]);
         }
 
-        if (exp[0] == "*"){
+        if (exp[0] === "*"){
             return this.eval(exp[1]) * this.eval(exp[2]);
         }
 
 
+        // -----------------------------
+        // variables declration
+        if (exp[0] === "var"){
+            const [_, name, value] = exp;
+            return env.define(name, this.eval(value));
+        }
 
-        
-        throw "Umimplemented"
+        // -----------------------------
+        // variables lookup
+        if (isVariableName(exp)){
+            return env.lookup(exp);
+        }
+
+        throw `Umimplemented: ${JSON.stringify(exp)}`
     }
 }
 
@@ -41,12 +55,34 @@ function isString(exp)
     return typeof exp === "string" && exp[0] === '"' && exp.slice(-1) == '"'
 }
 
-const eva = new Eva();
+function isVariableName(exp)
+{
+    return typeof exp === "string" && /^[a-zA-Z][a-zA-Z0-9_]*$/.test(exp);
+}
+
+
+const env = new Environment({
+    null: null,
+
+    true: true,
+    false: false,
+
+    VERSION: '0.1',
+});
+const eva = new Eva(env);
+
 assert.strictEqual(eva.eval(1), 1);
 assert.strictEqual(eva.eval('"Hello"'), 'Hello');
 assert.strictEqual(eva.eval(["+", 1, 2]), 3);
 assert.strictEqual(eva.eval(["+", ["+", 1, 2], 2]), 5);
 assert.strictEqual(eva.eval(["*", ["+", 1, 2], 2]), 6);
 
+
+assert.strictEqual(eva.eval(["var", "x", 2]), 2);
+assert.strictEqual(eva.eval("x"), 2);
+
+
+assert.strictEqual(eva.eval("VERSION"), "0.1");
+assert.strictEqual(eva.eval(["var", "isUser", "true"]), true);
 
 console.log("All assertions passed!")
